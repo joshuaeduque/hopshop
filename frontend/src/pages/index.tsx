2c69fd5f35f70840947853d95d744973685fdf6a
Josh <joshuaeduque@gmail.com>
2025-01-25 20:55:25 -0600

    Made an API call from the frontend

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,5 +4,13 @@ export default function Home() {
   return (<div>
     <p>Home Page</p>
     <Link href={"/about"} className="underline">About</Link>
+
+    <button onClick={() => {
+      fetch('http://localhost:8000/neons')
+        .then(response => response.json())
+        .then((json) => { console.log(json) })
+        .finally(() => { console.log('finished') })
+        .catch((reason) => { console.error(reason) });
+    }}>Click me</button>
   </div>);
 }
