cc71b0250b347c3c0c7a172f3a8e65a3013d5043
Josh <joshuaeduque@gmail.com>
2025-02-13 15:40:22 -0600

    Added site header and sort bar

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,25 +1,19 @@
-import Link from "next/link";
+import SiteHeader from "@/components/SiteHeader/SiteHeader";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-green-300 via-green-400 to-green-500 flex items-center justify-center">
-      <div className="text-center p-10 bg-white bg-opacity-70 rounded-lg shadow-lg">
-        <h1 className="text-5xl font-extrabold text-green-900 mb-4">Welcome to HopShop</h1>
-        <p className="text-lg text-green-800 mb-6">Home Page</p>
-        <Link href="/about">
-          <span className="text-green-900 font-bold">About</span>
-        </Link>
-        <button onClick={() => {
-          console.log('making api request...');
-
-          fetch('http://localhost:8000/neons')
-            .then(response => response.json())
-            .then((json) => { console.log(json) })
-            .finally(() => { console.log('finished') })
-            .catch((reason) => { console.error(reason) });
-        }} className="text-red-500">Click me</button>
+    <div>
+      <SiteHeader />
+      {/* Product filtering bar */}
+      <div className="bg-base-200 px-4 flex">
+        <div className="dropdown dropdown-end ml-auto">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-sm m-1">Sort by</div>
+          <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+            <li><a>Price: Highest to Lowest</a></li>
+            <li><a>Price: Lowest to Highest</a></li>
+          </ul>
+        </div>
       </div>
     </div>
   );
-
 }
