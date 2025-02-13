717b18f3f35e7f6153b1bd0988b5c365acd83ff3
Josh <joshuaeduque@gmail.com>
2025-02-13 16:26:20 -0600

    Replaced sort bar with categories bar

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,15 +4,29 @@ export default function Home() {
   return (
     <div className="flex flex-col">
       <SiteHeader />
-      {/* Product filtering bar */}
-      <div className="bg-base-200 px-4 flex">
-        <div className="dropdown dropdown-end ml-auto">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-sm m-1">Sort by</div>
-          <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-            <li><a>Price: Highest to Lowest</a></li>
-            <li><a>Price: Lowest to Highest</a></li>
-          </ul>
-        </div>
+      {/* Categories bar */}
+      <div className="bg-base-200 px-4 flex items-center gap-2">
+        <button className="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block h-5 w-5 stroke-current">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
+        </button>
+        <button className="btn btn-ghost btn-sm">Pet frogs</button>
+        <button className="btn btn-ghost btn-sm">Live Insects</button>
+        <button className="btn btn-ghost btn-sm">Heat & Lighting</button>
+        <button className="btn btn-ghost btn-sm">Water & Humidity</button>
+        <button className="btn btn-ghost btn-sm">Decorations</button>
+        <button className="btn btn-ghost btn-sm">Supplements</button>
+        <button className="btn btn-ghost btn-sm">Apparel</button>
+        <button className="btn btn-ghost btn-sm">Books</button>
       </div>
     </div>
   );
