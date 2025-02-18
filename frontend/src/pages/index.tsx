3334230cc30c327bdcf1d08e0ef1946418b54a29
Josh <116848739+joshuaeduque@users.noreply.github.com>
2025-02-18 16:34:45 -0600

    Homepage scaffolding (#4)

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,25 +1,42 @@
-import Link from "next/link";
+import CategoriesBar from "@/components/CategoriesBar/CategoriesBar";
+import SiteHeader from "@/components/SiteHeader/SiteHeader";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-gradient-to-r from-green-300 via-green-400 to-green-500 flex items-center justify-center">
-      <div className="text-center p-10 bg-white bg-opacity-70 rounded-lg shadow-lg">
-        <h1 className="text-5xl font-extrabold text-green-900 mb-4">Welcome to HopShop</h1>
-        <p className="text-lg text-green-800 mb-6">Home Page</p>
-        <Link href="/about">
-          <span className="text-green-900 font-bold">About</span>
-        </Link>
-        <button onClick={() => {
-          console.log('making api request...');
 
-          fetch('http://localhost:8000/neons')
-            .then(response => response.json())
-            .then((json) => { console.log(json) })
-            .finally(() => { console.log('finished') })
-            .catch((reason) => { console.error(reason) });
-        }} className="text-red-500">Click me</button>
+  const products = [
+    {
+      name: 'Pacman frog',
+      price: 99
+    },
+    {
+      name: 'Budget frog',
+      price: 199
+    },
+    {
+      name: 'Tree frog',
+      price: 299
+    }
+  ];
+
+  return (
+    <div className="flex flex-col min-w-max">
+      <SiteHeader />
+      <CategoriesBar />
+      <div className="p-4 flex flex-wrap gap-4">
+        {products.map((product) => {
+          return (
+            <div className="card bg-base-100 h-60 w-60 shadow-xl">
+              <figure>
+                <img src="/FROG_GANG.jpg"/>
+              </figure>
+              <div className="card-body">
+                <p className="card-title">{product.name}</p>
+                <p>${product.price / 100}</p>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   );
-
 }
