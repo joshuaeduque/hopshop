d8f15db0052a4b4f790872b56f6757fd28893eb2
Josh <joshuaeduque@gmail.com>
2025-02-25 16:49:31 -0600

    More product page work

--- frontend/src/pages/product.tsx
+++ frontend/src/pages/product.tsx
@@ -2,6 +2,14 @@ import CategoriesBar from "@/components/CategoriesBar/CategoriesBar";
 import SiteHeader from "@/components/SiteHeader/SiteHeader";
 
 export default function Product() {
+
+    const product = {
+        name: 'Crazy Cool Frog Thing',
+        rating: 5,
+        price: 99,
+        details: 'This is a crazy cool frog thing, it can do absolutely anything.'
+    };
+
     return (
         <div>
             <SiteHeader />
@@ -13,22 +21,19 @@ export default function Product() {
                         <li>product_category</li>
                     </ul>
                 </div>
-                <div className="grid grid-cols-4 gap-4">
-                    <div className="col-span-1">
-                        <img className="aspect-square object-cover" src="/FROG_GANG.jpg" />
+                <div className="grid grid-cols-2 gap-4 m-2">
+                    <div className="col-span-1 justify-self-center">
+                        <img className="aspect-square object-cover h-96 rounded" src="/FROG_GANG.jpg" />
                     </div>
-                    <div className="col-span-3">
-                        <p className="text-3xl">product_name</p>
-                        <p>product_rating</p>
-                        <hr className="border-neutral" />
-                        <p className="text-2xl my-2">$product_price</p>
-                        <p className="font-bold">Product details</p>
-                        <p>product_details</p>
-                        <div className="my-4">
-                                <label className="label-text font-bold" htmlFor="quantity">QTY:</label>
-                                <input defaultValue={0} min={0} className="input input-bordered mx-4 w-16" type="number" name="quantity" id="quantity" />
-                                <button className="btn">Add to cart</button>
-                            </div>
+                    <div className="col-span-1 flex flex-col ">
+                        <p className="text-4xl">{product.name}</p>
+                        <p className="w-96">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis explicabo nulla quos quisquam enim, corrupti sapiente. Autem repellat, ex odio alias perspiciatis, minus cum, cumque sed quae quaerat eos nulla?</p>
+                        <p className="text-xl">${product.price / 100}</p>
+                        <div className="flex flex-row gap-4">
+                            <label className="label-text font-bold content-center" htmlFor="quantity">QTY:</label>
+                            <input defaultValue={0} min={0} className="input input-bordered input-sm w-16" type="number" name="quantity" id="quantity" />
+                            <button className="btn btn-sm btn-wide">Add to cart</button>
+                        </div>
                     </div>
                 </div>
             </div>
