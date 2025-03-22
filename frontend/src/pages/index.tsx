b130490988d08e6e9bc3ad6d5fbc90e5bc3f0929
khenderson20 <kevin.d.hende@gmail.com>
2025-03-22 18:30:20 -0500

    added daisyUI and react-icons

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -7,6 +7,8 @@ export default function Home() {
       <Navigation />
       <main className="min-h-screen p-4">
         {/* Main content goes here */}
+
+
         <h1 className="text-4xl font-bold">Welcome to HopShop</h1>
       </main>
       <Footer />
