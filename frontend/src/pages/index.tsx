fb5bccca61a46639c2e94b5e776e9012553d6996
Josh <116848739+joshuaeduque@users.noreply.github.com>
2025-03-22 15:11:10 -0500

    Featured and hot products on homepage (#9)

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import CategoriesBar from "@/components/CategoriesBar/CategoriesBar";
+import ProductCard from "@/components/ProductCard/ProductCard";
 import SiteHeader from "@/components/SiteHeader/SiteHeader";
 
 export default function Home() {
@@ -6,6 +7,21 @@ export default function Home() {
     <div>
       <SiteHeader />
       <CategoriesBar />
+
+      <div className="p-4 flex flex-col gap-4 justify-self-center">
+        <p className="font-bold">Featured Items</p>
+        <div className="flex flex-wrap gap-4">
+          {Array.from({ length: 4 }, (_, index) => (
+            <ProductCard name={`Product ${index}`} />
+          ))}
+        </div>
+        <p className="font-bold">Hot Items</p>
+        <div className="flex flex-wrap gap-4">
+          {Array.from({ length: 4 }, (_, index) => (
+            <ProductCard name={`Product ${index}`} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
