8b852d289c2d4d5387e8ccd62d30712d0876b540
Josh <joshuaeduque@gmail.com>
2025-04-02 23:29:35 -0500

    Changed default rating

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -39,14 +39,14 @@ export default function Home() {
                   <div className="rating rating-sm rating-half">
                     <input type="radio" name={`rating-${i}`} className="mask mask-star-2 mask-half-1 bg-orange-400" aria-label="0.5 star" />
                     <input type="radio" name={`rating-${i}`} className="mask mask-star-2 mask-half-2 bg-orange-400" aria-label="1 star" />
-                    <input type="radio" name={`rating-${i}`} className="mask mask-star-2 mask-half-1 bg-orange-400" aria-label="1.5 star" defaultChecked />
+                    <input type="radio" name={`rating-${i}`} className="mask mask-star-2 mask-half-1 bg-orange-400" aria-label="1.5 star" />
                     <input type="radio" name={`rating-${i}`} className="mask mask-star-2 mask-half-2 bg-orange-400" aria-label="2 star" />
                     <input type="radio" name={`rating-${i}`} className="mask mask-star-2 mask-half-1 bg-orange-400" aria-label="2.5 star" />
                     <input type="radio" name={`rating-${i}`} className="mask mask-star-2 mask-half-2 bg-orange-400" aria-label="3 star" />
                     <input type="radio" name={`rating-${i}`} className="mask mask-star-2 mask-half-1 bg-orange-400" aria-label="3.5 star" />
                     <input type="radio" name={`rating-${i}`} className="mask mask-star-2 mask-half-2 bg-orange-400" aria-label="4 star" />
                     <input type="radio" name={`rating-${i}`} className="mask mask-star-2 mask-half-1 bg-orange-400" aria-label="4.5 star" />
-                    <input type="radio" name={`rating-${i}`} className="mask mask-star-2 mask-half-2 bg-orange-400" aria-label="5 star" />
+                    <input type="radio" name={`rating-${i}`} className="mask mask-star-2 mask-half-2 bg-orange-400" aria-label="5 star" defaultChecked />
                   </div>
                   <p>$999</p>
                 </div>
